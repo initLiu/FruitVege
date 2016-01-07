353af39122cb49d3d28abb6bf315c9a07f109234
liuzhp <liuzp90@163.com>
2016-01-07 22:37:47 +0800

    commit

--- FruitVegeMIS/src/com/neusoft/fruitvegemis/activity/OrderFragment.java
+++ FruitVegeMIS/src/com/neusoft/fruitvegemis/activity/OrderFragment.java
@@ -1,13 +1,20 @@
 package com.neusoft.fruitvegemis.activity;
 
+import com.neusoft.fruitvegemis.R;
+import com.neusoft.fruitvegemis.adapter.OrderAdapter;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class OrderFragment extends Fragment {
 	public static final String TAG = "OrderFragment";
+
+	private ListView mOrderList;
+	private OrderAdapter mAdapter;
 
 	public static OrderFragment getInstance() {
 		OrderFragment fragment = new OrderFragment();
@@ -23,13 +30,23 @@ public class OrderFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		return super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.fragment_order, null);
+		return view;
 	}
 
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onViewCreated(view, savedInstanceState);
+		initUI(view);
+		initData();
+	}
+
+	private void initUI(View view) {
+		mOrderList = (ListView) view.findViewById(R.id.orderList);
+	}
+
+	private void initData() {
+		mAdapter = new OrderAdapter(getActivity());
+		mOrderList.setAdapter(mAdapter);
 	}
 }
